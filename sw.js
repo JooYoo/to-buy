70cb9313a536414d3efd665f31ef2c03d2eb680f
JooYooGit <jimmy.zhuyu@gmail.com>
2018-05-31 06:26:00 +0200

    added all file in sw

--- sw.js
+++ sw.js
@@ -12,15 +12,27 @@ self.addEventListener('install', function(event) {
       return cache.addAll([
         '/',
         'index.html',
+        'CNAME',
         'js/app.js',
         'js/jquery.min.js',
         'js/note-list.js',
+        'js/cache-polyfill.js',
         'css/style.css',
         'favicon.ico',
         'manifest.json',
         'img/icon-60.png',
         'img/icon-114.png',
-        'img/icon-152.png'
+        'img/icon-152.png',
+        'img/icon-200.png',
+        'img/apple-icon-57x57.png',
+        'img/apple-icon-60x60.png',
+        'img/apple-icon-72x72.png',
+        'img/apple-icon-76x76.png',
+        'img/apple-icon-114x114.png',
+        'img/apple-icon-120x120.png',
+        'img/apple-icon-144x144.png',
+        'img/apple-icon-152x152.png',
+        'img/apple-icon-180x180.png'
       ]);
     })
   );
