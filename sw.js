b8fd84e2648fd1b6f83dcb5c984b394ec3346f2f
JooYooGit <jimmy.zhuyu@gmail.com>
2018-05-31 11:41:29 +0200

    exchanged sw addEventListener

--- sw.js
+++ sw.js
@@ -38,16 +38,42 @@ self.addEventListener('install', function(event) {
 });
 
 // Use ServiceWorker (or not) to fetch data
-self.addEventListener('fetch', function(event) {
+// self.addEventListener('fetch', function(event) {
 
+//   event.respondWith(
+
+//     // Look for something in the cache that matches the request
+//     caches.match(event.request).then(function(response) {
+
+//       // If we find something, return it
+//       // Otherwise, use the network instead
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+addEventListener('fetch', function(event) {
   event.respondWith(
-
-    // Look for something in the cache that matches the request
-    caches.match(event.request).then(function(response) {
-
-      // If we find something, return it
-      // Otherwise, use the network instead
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;     // if valid response is found in cache return it
+        } else {
+          return fetch(event.request)     //fetch from internet
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());    //save the response for future
+                  return res;   // return the fetched data
+                })
+            })
+            .catch(function(err) {       // fallback mechanism
+              return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
+                .then(function(cache) {
+                  return cache.match('/offline.html');
+                });
+            });
+        }
+      })
   );
 });
