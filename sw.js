8ed68d1976c56d15204c010b7092319e13c41e3c
JooYooGit <jimmy.zhuyu@gmail.com>
2018-05-31 21:05:30 +0200

    change to old sw

--- sw.js
+++ sw.js
@@ -38,19 +38,19 @@ self.addEventListener('install', function(event) {
 });
 
 // Use ServiceWorker (or not) to fetch data
-// self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
 
-//   event.respondWith(
+  event.respondWith(
 
-//     // Look for something in the cache that matches the request
-//     caches.match(event.request).then(function(response) {
+    // Look for something in the cache that matches the request
+    caches.match(event.request).then(function(response) {
 
-//       // If we find something, return it
-//       // Otherwise, use the network instead
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
+      // If we find something, return it
+      // Otherwise, use the network instead
+      return response || fetch(event.request);
+    })
+  );
+});
 
 // addEventListener('fetch', function(event) {
 //   event.respondWith(
@@ -78,51 +78,52 @@ self.addEventListener('install', function(event) {
 //   );
 // });
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(cache_name)
-    .then(function(cache) {
-      return cache.addAll(cached_urls);
-    })
-  );
-});
+// 离线可见，但不可用
+// self.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open(cache_name)
+//     .then(function(cache) {
+//       return cache.addAll(cached_urls);
+//     })
+//   );
+// });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName.startsWith('pages-cache-') && staticCacheName !== cacheName) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+// self.addEventListener('activate', function(event) {
+//   event.waitUntil(
+//     caches.keys().then(function(cacheNames) {
+//       return Promise.all(
+//         cacheNames.map(function(cacheName) {
+//           if (cacheName.startsWith('pages-cache-') && staticCacheName !== cacheName) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
 
-self.addEventListener('fetch', function(event) {
-    console.log('Fetch event for ', event.request.url);
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        if (response) {
-          console.log('Found ', event.request.url, ' in cache');
-          return response;
-        }
-        console.log('Network request for ', event.request.url);
-        return fetch(event.request).then(function(response) {
-          if (response.status === 404) {
-            return caches.match('fourohfour.html');
-          }
-          return caches.open(cached_urls).then(function(cache) {
-           cache.put(event.request.url, response.clone());
-            return response;
-          });
-        });
-      }).catch(function(error) {
-        console.log('Error, ', error);
-        return caches.match('offline.html');
-      })
-    );
-  });
+// self.addEventListener('fetch', function(event) {
+//     console.log('Fetch event for ', event.request.url);
+//     event.respondWith(
+//       caches.match(event.request).then(function(response) {
+//         if (response) {
+//           console.log('Found ', event.request.url, ' in cache');
+//           return response;
+//         }
+//         console.log('Network request for ', event.request.url);
+//         return fetch(event.request).then(function(response) {
+//           if (response.status === 404) {
+//             return caches.match('fourohfour.html');
+//           }
+//           return caches.open(cached_urls).then(function(cache) {
+//            cache.put(event.request.url, response.clone());
+//             return response;
+//           });
+//         });
+//       }).catch(function(error) {
+//         console.log('Error, ', error);
+//         return caches.match('offline.html');
+//       })
+//     );
+//   });
  
