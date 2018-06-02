2ee11f4418baeded5c5dd53b65e1e4775adcbca1
JooYooGit <jimmy.zhuyu@gmail.com>
2018-06-02 15:52:28 +0200

    cache sw

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 importScripts('js/cache-polyfill.js');
 
 // Install the ServiceWorker
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   event.waitUntil(
 
     // Open a cache
-    caches.open('v1').then(function(cache) {
+    caches.open('v1').then(function (cache) {
 
       // Define what we want to cache
       return cache.addAll([
         '/',
         'index.html',
-        'CNAME',
+        'sw.js',
         'js/app.js',
         'js/jquery.min.js',
         'js/note-list.js',
@@ -37,16 +37,16 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('activate',function(event){
+self.addEventListener('activate', function (event) {
   console.log("[Service Worker] activating service Worker", event);
   return self.clients.claim();
 });
 
 // Use ServiceWorker (or not) to fetch data
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     // Look for something in the cache that matches the request
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       // If we find something, return it
       // Otherwise, use the network instead
       return response || fetch(event.request);
@@ -128,4 +128,4 @@ self.addEventListener('fetch', function(event) {
 //       })
 //     );
 //   });
- 
+
